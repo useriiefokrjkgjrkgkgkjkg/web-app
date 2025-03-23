b45273ac034d1882c097ab65f988ad882d6c9217
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-23 22:35:58 +0300

    Add Vercel configuration

--- telegram-mini-app/next.config.ts
+++ telegram-mini-app/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 export default nextConfig;
